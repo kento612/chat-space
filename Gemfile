029634d6966e105b8e2770da447f8002593a654f
kento612 <kentohoriuchi@yahoo.co.jp>
2020-03-10 16:02:22 +0900

    updated_chatspace

--- Gemfile
+++ Gemfile
@@ -54,3 +54,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'haml-rails'
 gem 'font-awesome-sass'
 gem 'devise'
+gem 'pry-rails'
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :name, presence: true, uniqueness: true
-  
+  has_many :group_users
+  has_many :groups, through: :group_users
+  # validates :name, presence: true, uniqueness: true
+
 end

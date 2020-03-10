0946b4638de790aaa2571dd91738f43705c4f6b6
kento612 <kentohoriuchi@yahoo.co.jp>
2020-03-10 20:56:21 +0900

    updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
 
   has_many :group_users
   has_many :groups, through: :group_users
+  has_many :messages
   # validates :name, presence: true, uniqueness: true
 
 end

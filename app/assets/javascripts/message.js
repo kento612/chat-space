3884a1263cdd1388fa7bca70d44a07f540c40136
kento612 <kentohoriuchi@yahoo.co.jp>
2020-03-17 18:16:50 +0900

    update

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -72,7 +72,6 @@ $(function(){
 
   var reloadMessages = function() {
     var last_message_id = $('.main__messages__data:last').data("message-id");
-    console.log(last_message_id)
     $.ajax({
       url: "api/messages",
       type: 'get',

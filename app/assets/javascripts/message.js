b203c8d6e67f08ac703a06a703ca17117e54fc64
kento612 <kentohoriuchi@yahoo.co.jp>
2020-03-17 17:53:53 +0900

    auto_refresh

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,37 +1,51 @@
 $(function(){
   function buildHTML(message){
-    if ( message.image ) {
+    if (message.content && message.image) {
       var html =
-      `<div class="main__messages__data">
-          <div class="main__messages__data__who">
-            ${message.user_name}
-          </div>
-          <div class="main__messages__data__when">
-            ${message.created_at}
-          </div>
-        </div>
-        <div class="main__messages__message">
-          ${message.content}
-        </div>
-        <img class="lower-message__image" src=${message.image}>
-      </div>`
-    return html;
-  } else {
+      `<div class="main__messages__data" data-message-id=` + message.id + `>` +
+          `<div class="main__messages__data__who">` +
+            message.user_name +
+          `</div>` +
+          `<div class="main__messages__data__when">` +
+            message.created_at +
+          `</div>` +
+        `</div>`
+        `<p class="main__messages__message">` +
+            message.content +
+            `</p>` +
+        `<img src"` + message.image + `"class="lower-message__image" >` +
+      `</div>` +
+    `</div>` 
+  } else if (message.content) {
     var html =
-      `<div class="main__messages__data">
-          <div class="main__messages__data__who">
-            ${message.user_name}
-          </div>
-          <div class="main__messages__data__when">
-            ${message.created_at}
-          </div>
-        </div>
-          <div class="main__messages__message">
-            ${message.content}
-        </div>`
-      return html;
-    };
-  }
+      `<div class="main__messages__data" data-message-id=` + message.id + `>` +
+          `<div class="main__messages__data__who">` +
+            message.user_name +
+          `</div>` +
+          `<div class="main__messages__data__when">` +
+            message.created_at +
+          `</div>` +
+        `</div>` +
+        `<p class="main__messages__message">` +
+            message.content +
+          `</p>` +
+        `</div>` +
+      `</div>`
+    } else if (message.image) {
+      `<div class="main__messages__data" data-message-id=` + message.id + `>` +
+        `<div class="main__messages__data__who">` +
+          message.user_name +
+      `</div>` +
+      `<div class="main__messages__data__when">` +
+        message.created_at +
+      `</div>` +
+    `<img src"` + message.image + `"class="lower-message__image" >` +
+  `</div>` +
+`</div>` 
+  };
+  return html;
+};
+
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this);
@@ -51,5 +65,35 @@ $(function(){
       $('.main__messages').animate({ scrollTop: $('.main__messages')[0].scrollHeight});
       $('.form__send').attr('disabled');
     })
-})
+    .fail(function(){
+      alert('error');
+    })
+  });
+
+  var reloadMessages = function() {
+    var last_message_id = $('.main__messages__data:last').data("message-id");
+    console.log(last_message_id)
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.main__messages').append(insertHTML);
+      $('.main__messages').animate({ scrollTop: $('.main__messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 7000);
+  }
 });
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root "groups#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
+
+    namespace :api do
+      resources :messages, only: :index, defaults: { format: 'json' }
+    end
   end
 end

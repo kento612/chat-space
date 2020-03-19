6bab67457326342e2475dd15d657d7a8143e742c
kento612 <kentohoriuchi@yahoo.co.jp>
2020-03-19 11:45:35 +0900

    updated

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ lock '3.12.1'
 set :application, 'chat-space'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:kento612/chats-pace.git'
+set :repo_url,  'git@github.com:kento612/chat-space.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
